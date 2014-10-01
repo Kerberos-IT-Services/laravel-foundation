fb0850b8fea23dbfe3113e735907ec83f83ae12c
alecz2303 <alecz23@live.com>
2014-10-01 14:47:58 -0500

    Update for Pagoda Box

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -5,7 +5,7 @@ class UserTableSeeder extends Seeder {
   public function run()
   {
     $user = new User;
-    $user->username = 'prueba';
+    $user->username = 'admin';
     $user->email = 'admin@site.dev';
     $user->password = 'admin';
     $user->password_confirmation = 'admin';
@@ -15,6 +15,21 @@ class UserTableSeeder extends Seeder {
     if(! $user->save()) {
       Log::info('Unable to create user '.$user->username, (array)$user->errors());
     } else {
+      $admin = new Role;
+    $admin->name = 'Admin';
+    $admin->save();
+
+    $user = User::where('username','=','admin')->first();
+
+    /* role attach alias */
+    $user->attachRole( $admin ); // Parameter can be an Role object, array or id.
+
+    $manageUsers = new Permission;
+    $manageUsers->name = 'manage_users';
+    $manageUsers->display_name = 'Manage Users';
+    $manageUsers->save();
+
+    $admin->perms()->sync(array($manageUsers->id));
       Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
     }
   }
